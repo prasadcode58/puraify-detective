37f790894f85867fe3d4a4e6e7f93280c6ef91a9
prasadcode58 <prasadcode58@gmail.com>
2024-02-09 10:29:36 +0530

    uploading project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+    },
+    fontFamily: {
+      spacemono: ["Space Mono", 'monospace'],
+    }
+  },
+  plugins: [],
+}
+
